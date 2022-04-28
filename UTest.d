bbedbe605401c4ef4ebd5e6a2a4c98e572e690b5
Marie <marie.quelo@insa-rennes.fr>
2022-04-28 17:24:44 +0200

    delete .o

--- UTest.d
+++
@@ -1,3 +0,0 @@
-UTest.o: UTest.c Controller/controller.h View/main_view.h \
- Model/main_model.h Model/object.h Model/personnage.h Model/graph.h \
- Model/main_model.h
--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.c Controller/controller.h View/main_view.h \
- Model/main_model.h Model/object.h Model/personnage.h Model/graph.h \
- Model/main_model.h
